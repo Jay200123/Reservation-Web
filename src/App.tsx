7f0befed8f85c41823f98f3df6082e8f65284de4
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-29 20:18:59 +0800

    Add App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h3 className=" lg:text-shadow-blue-500 md:text-red-500 text-yellow-500">
+        Hello World
+      </h3>
+    </>
+  );
+}
+
+export default App;
