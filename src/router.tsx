038630125ec0e7465b561dc5a86fc5c00790f85b
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-29 20:51:58 +0800

    Add router.tsx file for Page Routes.

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const Router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            {/* Routes here */}
+        </Route>
+    )
+);
+
+export default Router;
