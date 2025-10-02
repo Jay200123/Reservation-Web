709a3cb98dd3fb654f4bc2bc3b24a14c1045af20
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-02 13:52:10 +0800

    Add route for default Home page wrapped with a MotionWrapper for motion animations.

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,25 @@
 import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
 } from "react-router-dom";
+import { MotionWrapper } from "./@components";
+import { HomeLayout } from "./@layouts";
+import { Home } from "./@pages";
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
-            {/* Routes here */}
+            {/*Public Routes here */}
+            <Route element={<HomeLayout />}>
+                <Route index path="/" element={
+                    <MotionWrapper>
+                        <Home />
+                    </MotionWrapper>
+                } />
+            </Route>
+
+            {/* Private Routes here */}
         </Route>
     )
 );
