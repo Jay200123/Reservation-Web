0c6b9a9d1ec6bd2e269445f89fdc70f8cb791271
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-06 20:54:06 +0800

    Fixed SignUp ui component page

---
+++ src/@pages/SignUp/index.tsx
@@ -0,0 +1,83 @@
+import ImageOne from "../../assets/ReservationOne.avif";
+
+export default function SignUp() {
+    return (
+        <div className="min-h-screen flex justify-center items-center bg-[#d4af37]">
+            <div className="lg:w-[70rem] lg:h-[55rem] flex rounded-lg bg-white shadow-lg">
+                <div className="lg:w-1/2">
+                    <img src={ImageOne} className="w-full h-full object-cover rounded-lg" alt="ImageOne" />
+                </div>
+                <div className="lg:w-1/2 flex flex-col justify-center">
+                    <form className=" lg:px-3.5 lg:py-3.5">
+                        <h3 className="lg:text-4xl md:text-lg font-medium">Create Account</h3>
+                        <p className="lg:text-base">Join us today and start your journey.</p>
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="fullname">Fullname</label>
+                            <div className="relative">
+                                <i className="fa-regular fa-user absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="fullname" name="fullname" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="John Doe" />
+                            </div>
+                        </div>
+
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="username">Username</label>
+                            <div className="relative">
+                                <i className="fa-regular fa-user absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="username" name="username" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="john123" />
+                            </div>
+                        </div>
+
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="password">Password</label>
+                            <div className="relative">
+                                <i className="fa-solid fa-lock absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="password" name="password" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="*****" />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="email">Email</label>
+                            <div className="relative">
+                                <i className="fa-regular fa-envelope absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="email" name="email" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="john.doe@gmail.com" />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="contact_number">Contact Number</label>
+                            <div className="relative">
+                                <i className="fa-solid fa-phone absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="contact_number" name="contact_number" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="+63 9123456789" />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="address">Address</label>
+                            <div className="relative">
+                                <i className="fa-solid fa-location-dot absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="address" name="address" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="Block 115 Lot 8 Juan Street, etc." />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col p-2.5">
+                            <label htmlFor="city">City</label>
+                            <div className="relative">
+                                <i className="fa-solid fa-city absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
+                                <input type="text" id="city" name="city" className="p-1.5 border border-gray-400 w-full rounded-md pr-3 pl-10" placeholder="Taguig City..." />
+                            </div>
+                        </div>
+                        <div className="w-full flex justify-center items-center p-2.5">
+                            <button className="rounded-2xl border border-white bg-[#d4af37] text-white lg:text-base md:text-sm text-xs p-1 lg:px-2.5 lg:py-2.5 w-full md:px-1.5 md:py-1.5 lg:[8rem] lg:font-medium md:mb-2.5 mb-1.5 cursor-pointer">Sign Up</button>
+                        </div>
+
+                        <p className="text-base text-center">Already have an account?<span className="text-[#d4af37] underline">Sign In</span></p>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
