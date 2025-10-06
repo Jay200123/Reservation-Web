80a6c72c52c5013d252b00513a0c8c2e0d98d521
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-06 20:54:51 +0800

    Set up basicApi axios config

--- src/@state/api/index.ts
+++ src/@state/api/index.ts
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+const basicApi = axios.create({
+  baseURL: `${import.meta.env.BASE_URL}/api/v1`,
+  auth: {
+    username: import.meta.env.BASIC_AUTH_USERNAME,
+    password: import.meta.env.BASIC_AUTH_PASSWORD,
+  },
+});
+
+export { basicApi };
