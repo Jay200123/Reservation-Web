3d1a07f767c22b564f3cb005865311d10bd5d0b4
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-08 22:37:31 +0800

    Fixed UI Footer responsiveness.

--- src/@components/Footer/index.tsx
+++ src/@components/Footer/index.tsx
@@ -1,54 +1,52 @@
 export default function Footer() {
-
-
     const year = new Date().toISOString().split("T")[0];
 
     return (
         <>
-            <footer className={`w-full lg:h-[21rem] md:h-[18rem] h-full lg:p-3 bg-gray flex flex-col justify-between bg-[#333333] text-white`}>
-                <div className=" flex lg:flex-row justify-between lg:h-[12rem]">
+            <footer className={`w-full lg:h-[21rem] md:h-[18rem] h-[33rem] lg:p-3 bg-gray flex flex-col lg:justify-between md:justify-between bg-[#333333] text-white`}>
+                <div className=" flex lg:flex-row md:flex-row flex-col lg:justify-between md:justify-between justify-around lg:h-[12rem] md:h-[22rem] h-[28rem]">
                     <ul>
-                        <li className="font-bold lg:text-lg md:text-lg lg:mt-1.5">Primary Information</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">Reservation Web</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">About</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">123 Lorem St., Ipsum City, Dolor 4567
+                        <li className="font-bold lg:text-lg md:text-lg text-base lg:m-1.5 md:m-1.5 m-1">Primary Information</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">Reservation Web</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">About</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">123 Lorem St., Ipsum City, Dolor 4567
                         </li>
-                        <li className="lg:text-base md:text-base lg:mt-2">8:00 A.M to 5:00 P.M (Monday - Saturdays)</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">8:00 A.M to 5:00 P.M (Monday - Saturdays)</li>
                     </ul>
 
                     <ul>
-                        <li className="font-bold lg:text-lg md:text-lg lg:mt-1.5">Contact </li>
-                        <li className="lg:text-base md:text-base lg:mt-2">+63 9123456789</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">lorem.ipsum@gmail.com</li>
+                        <li className="font-bold lg:text-lg md:text-lg text-base lg:m-1.5 md:m-1.5 m-1 cursor-pointer">Contact </li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">+63 9123456789</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">lorem.ipsum@gmail.com</li>
                     </ul>
 
                     <ul>
-                        <li className="font-bold lg:text-lg md:text-lg lg:mt-1.5">Quick Links</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">Sign Up</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">Sign In</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">Services</li>
+                        <li className="font-bold lg:text-lg md:text-lg text-base lg:m-1.5 md:m-1.5 m-1">Quick Links</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">Sign Up</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">Sign In</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">Services</li>
                     </ul>
 
 
                     <ul>
-                        <li className="font-bold lg:text-lg md:text-lg lg:mt-1.5">Legal</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">Privacy Policy</li>
-                        <li className="lg:text-base md:text-base lg:mt-2">Terms & Conditions.</li>
+                        <li className="font-bold lg:text-lg md:text-lg text-base lg:m-1.5 md:m-1.5 m-1">Legal</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">Privacy Policy</li>
+                        <li className="lg:text-base md:text-base text-sm lg:m-2 md:m-1.5 m-1 cursor-pointer">Terms & Conditions.</li>
                     </ul>
                 </div>
-                <div className=" lg:h-12 lg:p-2 flex items-center">
-                    <i className="lg:text-2xl lg:mr-3.5 fa-brands fa-facebook"></i>
-                    <i className="lg:text-2xl lg:mr-3.5 fa-brands fa-instagram"></i>
-                    <i className="lg:text-2xl lg:mr-3.5 fa-brands fa-twitter"></i>
-                    <i className="lg:text-2xl lg:mr-3.5 fa-brands fa-google"></i>
+                <div className=" lg:h-12 lg:m-2 md:m-1.5 m-1 flex items-center">
+                    <i className="lg:text-2xl lg:m-3.5 md:text-lg text-base md:m-2.5 m-2 fa-brands fa-facebook cursor-pointer"></i>
+                    <i className="lg:text-2xl lg:m-3.5  md:text-lg text-base md:m-2.5 m-2 fa-brands fa-instagram cursor-pointer"></i>
+                    <i className="lg:text-2xl lg:m-3.5  md:text-lg text-base md:m-2.5 m-2 fa-brands fa-twitter cursor-pointer"></i>
+                    <i className="lg:text-2xl lg:m-3.5  md:text-lg text-base md:m-2.5 m-2 fa-brands fa-google cursor-pointer"></i>
                 </div>
                 <div className="flex justify-between border-t border-white">
-                    <div className="lg:mt-1.5">
-                        <p>Copyright {year.split("-")[0]} - Renyel Jay Sioc</p>
+                    <div className="lg:m-2 md:m-2 m-1.5">
+                        <p className="lg:text-lg md:text-base text-sm">Copyright {year.split("-")[0]} - Renyel Jay Sioc</p>
                     </div>
-                    <div className="lg:mt-1.5 flex items-center">
-                        <p>Privacy Policy.</p>
-                        <p>Terms & Condition.</p>
+                    <div className="lg:m-1.5 flex items-center">
+                        <p className="lg:text-lg md:text-base text-sm">Privacy Policy.</p>
+                        <p className="lg:text-lg md:text-base text-sm">Terms & Condition.</p>
                     </div>
                 </div>
             </footer>
