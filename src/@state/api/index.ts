2d9310e519384d208800857a43382511de905450
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-08 22:40:59 +0800

    Fixed typo on calling env variables.

--- src/@state/api/index.ts
+++ src/@state/api/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const basicApi = axios.create({
-  baseURL: `${import.meta.env.BASE_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
   auth: {
-    username: import.meta.env.BASIC_AUTH_USERNAME,
-    password: import.meta.env.BASIC_AUTH_PASSWORD,
+    username: import.meta.env.VITE_BASIC_AUTH_USERNAME,
+    password: import.meta.env.VITE_BASIC_AUTH_PASSWORD,
   },
 });
 
