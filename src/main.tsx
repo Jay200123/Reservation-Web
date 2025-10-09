bbcc5ed5dd2c3343a6e173650e95b2ddb98e1b10
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-09 23:13:27 +0800

    Add ToastContainer in main.tsx to enable global toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
+      <ToastContainer />
       <App />
     </QueryClientProvider>
   </StrictMode>
