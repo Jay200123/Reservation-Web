c7da1d18f2a0de1fdc245dac44f378afe70549fe
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-09 23:26:07 +0800

    Add `authApi` & `refreshApi` axios custom header configurations for accessing authenticated api's

--- src/@state/api/index.ts
+++ src/@state/api/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useStore } from "../store";
 
 const basicApi = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
@@ -8,4 +9,44 @@ const basicApi = axios.create({
   },
 });
 
-export { basicApi };
+const authApi = axios.create({
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+authApi.interceptors.request.use((config) => {
+  const access_token = useStore.getState().access_token;
+
+  if (access_token) {
+    config.headers["Authorization"] = `${access_token}`;
+  }
+
+  return config;
+});
+
+const refreshApi = axios.create({
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+refreshApi.interceptors.request.use((config) => {
+  const refresh_token = useStore.getState().refresh_token;
+
+  if (refresh_token) {
+    config.headers["Authorization"] = `${refresh_token}`;
+  }
+
+  return config;
+});
+
+export { 
+  basicApi, 
+  authApi,
+  refreshApi
+ };
