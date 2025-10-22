3408f19b4fa7a3fb639dcc09807512a6ede4c0b4
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-22 20:20:30 +0800

    Exprot ServiceDetails & ReservationForms pages.

--- src/@pages/index.ts
+++ src/@pages/index.ts
@@ -3,13 +3,17 @@ import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Profile from "./Profile";
 import EditProfile from "./EditProfile";
+import ServiceDetails from "./ServiceDetails";
+import ReservationForms from "./ReservationForms";
 import Dashboard from "./Dashboard";
 
-export { 
-    Home, 
-    SignIn, 
-    SignUp, 
-    Profile, 
-    EditProfile,
-    Dashboard
+export {
+  Home,
+  SignIn,
+  SignUp,
+  Profile,
+  EditProfile,
+  ServiceDetails,
+  ReservationForms,
+  Dashboard,
 };
