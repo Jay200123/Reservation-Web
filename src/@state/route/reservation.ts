2c6106b8c7e1a8492a528a2519ea9412f9b6b279
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-26 23:51:18 +0800

    Add useReservationStore hook for reservation crud operations

--- src/@state/route/reservation.ts
+++ src/@state/route/reservation.ts
@@ -1,10 +1,23 @@
 import type { StateCreator } from "zustand";
 import type { useReservationApi } from "../../@types";
+import { authApi } from "../api";
 
 export const useReservationStore: StateCreator<useReservationApi> = (_set) => ({
-  getAllReservations: async () => {},
-  getReservationById: async () => {},
-  createReservation: async () => {},
+  getAllReservations: async () => {
+    const result = await authApi.get("/reservations");
+
+    return result.data;
+  },
+  getReservationById: async (id: string) => {
+    const result = await authApi.get(`/reservation/${id}`);
+
+    return result.data;
+  },
+  createReservation: async (data: any) => {
+    const result = await authApi.post("/reservations", data);
+
+    return result.data;
+  },
   rescheduleReservation: async () => {},
   updateReservationStatus: async () => {},
 });
