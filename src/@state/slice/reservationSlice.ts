7b0b3499f89e19e45666d634a8880fbfb51a5583
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-26 23:51:48 +0800

    Add reservationSlice

---
+++ src/@state/slice/reservationSlice.ts
@@ -0,0 +1,48 @@
+import type { StateCreator } from "zustand";
+import type { Services, useReservationFormSlice } from "../../@types";
+
+export const useReservationSlice: StateCreator<useReservationFormSlice> = (
+  set
+) => ({
+  services: [],
+  timeslot: null,
+  reservation_date: null,
+  payment_type: "",
+
+  addServiceToForm: (service: Services) => {
+    set((state) => ({
+      services: [...state.services, service],
+    }));
+  },
+
+  removeServiceFromFrom: (serviceId: string) => {
+    set((state) => ({
+      services: state.services.filter((service) => service._id !== serviceId),
+    }));
+  },
+
+  addReservationDate: (date: string) => {
+    set({ reservation_date: date });
+  },
+
+  addTimeslot: (timeslotId: string) => {
+    set({
+      timeslot: timeslotId,
+    });
+  },
+
+  addPaymentType: (payment_type: string) => {
+    set({
+      payment_type: payment_type,
+    });
+  },
+
+  clearForm: () => {
+    set({
+      services: [],
+      timeslot: null,
+      reservation_date: null,
+      payment_type: "",
+    });
+  },
+});
