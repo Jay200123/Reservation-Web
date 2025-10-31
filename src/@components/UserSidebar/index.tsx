b0e9807fc387da6970a1e074da563fcad28cc673
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-31 20:10:53 +0800

    Add comments on UserSidebar component.

--- src/@components/UserSidebar/index.tsx
+++ src/@components/UserSidebar/index.tsx
@@ -3,9 +3,13 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 export default function UserSidebar() {
+    // Use the `useNavigate` hook from React Router for page navigation
     const navigate = useNavigate();
+
+    // Destructure user, services, and logout from the Zustand store
     const { user, services, logout } = useStore();
 
+    // Define an async `handleLogout` function to call the logout action and navigate to the sign-in page
     const handleLogout = async () => {
         await logout();
         toast.success("Logout successfully");
@@ -30,18 +34,22 @@ export default function UserSidebar() {
 
                 {/* Desktop Menu */}
                 <ul className="hidden md:flex flex-col gap-6 text-xs md:text-sm lg:text-sm">
+                    {/* Landing Page */}
                     <li onClick={() => navigate("/")} className="cursor-pointer transition-all duration-300 ease-in-out hover:bg-white hover:text-[#d4af37] lg:p-2.5 rounded-md font-medium">
                         <i className="fa-solid fa-house mr-1"></i>Home
                     </li>
-                    
+
+                    {/* Profile Page */}
                     <li onClick={() => navigate("/profile")} className="cursor-pointer  transition-all duration-300 hover:bg-white hover:text-[#d4af37] lg:p-2.5 rounded-md font-medium">
                         <i className="fa-solid fa-user mr-1"></i>Profile
                     </li>
 
+                    {/* Services Page */}
                     <li onClick={() => toast.error("Feature on progress")} className="cursor-pointer  transition-all duration-300 hover:bg-white hover:text-[#d4af37] lg:p-2.5 rounded-md font-medium">
                         <i className="fa-solid fa-user-gear mr-1"></i>Services
                     </li>
 
+                    {/* User Reservations page */}
                     <li onClick={() => navigate(`/reservations/${user?._id}`)} className="cursor-pointer  transition-all duration-300 hover:bg-white hover:text-[#d4af37] lg:p-2.5 rounded-md font-medium">
                         <i className="fa-solid fa-bookmark mr-1"></i>Reservations
                     </li>
