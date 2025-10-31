e53116781dee45a97757d1f7603b8586af62c3c4
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-31 10:53:29 +0800

    Add USER PATH constant

--- src/@state/route/user.ts
+++ src/@state/route/user.ts
@@ -1,22 +1,23 @@
 import type { StateCreator } from "zustand";
 import type { useUserApi, UserDetails } from "../../@types";
 import { authApi } from "../api";
+import { PATH } from "../../@constants";
 
 export const useUserStore: StateCreator<useUserApi> = (_set) => ({
   getAllUsers: async () => {
-    const result = await authApi.get("/users");
+    const result = await authApi.get(PATH.USERS);
 
     return result.data;
   },
 
   getUserById: async (id: string) => {
-    const result = await authApi.get(`user/${id}`);
+    const result = await authApi.get(PATH.USER_ID.replace(":id", id));
 
     return result.data;
   },
 
   updateUserById: async (id: string, data: Partial<UserDetails>) => {
-    return await authApi.patch(`/user/edit/${id}`, {
+    return await authApi.patch(PATH.EDIT_USER_ID.replace(":id", id), {
       data,
     });
   },
