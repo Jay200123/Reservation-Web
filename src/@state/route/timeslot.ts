bb51013ed2dd0733054be11b7cf845cdc6d2987f
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-31 10:53:21 +0800

    Add TIMESLOT PATH constant

--- src/@state/route/timeslot.ts
+++ src/@state/route/timeslot.ts
@@ -1,26 +1,33 @@
 import type { StateCreator } from "zustand";
 import type { useTimeslotApi } from "../../@types/state/timeslot";
 import { basicApi, authApi } from "../api";
+import { PATH } from "../../@constants";
 
 const useTimeslotStore: StateCreator<useTimeslotApi> = (_set) => ({
   getAllTimeslots: async () => {
-    const result = await basicApi.get("/timeslots");
+    const result = await basicApi.get(PATH.TIMESLOTS);
 
     return result.data;
   },
+
   getTimeslotById: async (id: string) => {
-    const result = await basicApi.get(`/timeslot/${id}`);
+    const result = await basicApi.get(PATH.TIMESLOT_ID.replace(":id", id));
 
     return result.data;
   },
+
   createTimeslot: async (data: FormData) => {
-    return await authApi.post("/timeslots", { data });
+    return await authApi.post(PATH.TIMESLOTS, { data });
   },
+
   updateTimeslotById: async (id: string, data: FormData) => {
-    return await authApi.post(`/timeslot/edit/${id}`, { data });
+    return await authApi.post(PATH.EDIT_TIMESLOT_ID.replace(":id", id), {
+      data,
+    });
   },
-  deleteTimeslotById: async (id: String) => {
-    return await authApi.delete(`/timeslot/${id}`);
+
+  deleteTimeslotById: async (id: string) => {
+    return await authApi.delete(PATH.TIMESLOT_ID.replace(":id", id));
   },
 });
 
