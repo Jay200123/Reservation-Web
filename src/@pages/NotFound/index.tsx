f4cbc1c416027440060d948e9c5cf9b303e846e3
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-09 20:32:58 +0800

    Add NotFound page

--- src/@pages/NotFound/index.tsx
+++ src/@pages/NotFound/index.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <h1 className="lg:text-6xl md:text-3xl text-2xl font-bold">
-                404 <span className="text-red-500">Not Found</span>
+        <div className="flex items-center justify-center min-h-screen bg-[#c9a128]">
+            <h1 className="lg:text-6xl md:text-3xl text-2xl font-bold text-white drop-shadow-lg">
+                404 <span className="text-red-600">Not Found</span>
             </h1>
         </div>
     )
