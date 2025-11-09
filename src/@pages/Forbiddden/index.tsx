ef9f19427a56e021de55f968914e288d49321e68
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-09 20:32:53 +0800

    Changed Forbidden page

--- src/@pages/Forbiddden/index.tsx
+++ src/@pages/Forbiddden/index.tsx
@@ -1,8 +1,8 @@
 export default function Forbidden() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <h1 className="lg:text-6xl md:text-3xl text-2xl font-bold">
-                403 <span className="text-red-500">Forbidden</span>
+        <div className="flex items-center justify-center min-h-screen bg-[#c9a128]">
+            <h1 className="lg:text-6xl md:text-3xl text-2xl font-bold text-white drop-shadow-lg">
+                403 <span className="text-red-600">Forbidden</span>
             </h1>
         </div>
     )
