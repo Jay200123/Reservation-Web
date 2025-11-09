77260f670f9b467769a6919b8b1ac900d5b2c7cb
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-09 20:21:29 +0800

    Add Forbidden page

---
+++ src/@pages/Forbiddden/index.tsx
@@ -0,0 +1,9 @@
+export default function Forbidden() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <h1 className="lg:text-6xl md:text-3xl text-2xl font-bold">
+                403 <span className="text-red-500">Forbidden</span>
+            </h1>
+        </div>
+    )
+}
