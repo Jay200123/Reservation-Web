f018ef1f3ca12699f38cebf3d9ccb49a0032b397
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-12 23:35:25 +0800

    Add React-datatable & user data on UsersTable using tanstack query & zustand `getAllUsers` action.

--- src/@pages/UsersTable/index.tsx
+++ src/@pages/UsersTable/index.tsx
@@ -1,7 +1,89 @@
+import { useStore } from "../../@state/store";
+import { useQuery } from "@tanstack/react-query";
+import DataTable, { type TableColumn } from "react-data-table-component";
+import { FadeLoader } from "react-spinners";
+import type { UserDetails } from "../../@types";
+import { tableCustomStyles } from "../../@utils";
+import { FaEye } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 export default function UsersTable() {
+    const navigate = useNavigate();
+
+    const { getAllUsers } = useStore();
+
+    const { data, isLoading } = useQuery({
+        queryKey: ["users"],
+        queryFn: getAllUsers,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchInterval: false
+    });
+
+    const users = data?.details ?? [];
+
+
+    const columns: TableColumn<UserDetails>[] = [
+        {
+            name: "ID",
+            selector: (row) => row.user._id
+        },
+        {
+            name: "Username",
+            selector: (row) => row?.user?.username
+        },
+        {
+            name: "Fullname",
+            selector: (row) => row?.fullname
+        },
+        {
+            name: "Email",
+            selector: (row) => row?.email,
+        },
+        {
+            name: "Contact Number",
+            selector: (row) => row?.contact_number
+        },
+        {
+            name: "Address",
+            selector: (row) => row?.address
+        },
+        {
+            name: "City",
+            selector: (row) => row?.city
+        },
+        {
+            name: "Actions",
+            cell: row => (
+                <div className="flex text-center items-center ">
+                    <FaEye className='mr-2 text-xl text-green-500'
+                        onClick={() => navigate(`/user/${row.user._id}`)}
+                    />
+                </div>
+            )
+        }
+    ];
+
     return (
-        <>
-            <h3>Users Table</h3>
-        </>
+        <div className="flex items-center justify-center min-h-screen">
+            {isLoading ? (
+                <FadeLoader color="#c9a128" />
+            ) : (
+                <div className="flex flex-col rounded-md shadow-lg">
+                    <DataTable
+                        title="Users Table"
+                        columns={columns}
+                        data={users}
+                        pagination
+                        highlightOnHover
+                        pointerOnHover
+                        paginationPerPage={10}
+                        paginationRowsPerPageOptions={[10, 20, 30]}
+                        customStyles={tableCustomStyles}
+                    />
+                </div>
+
+            )}
+        </div>
     )
 }
