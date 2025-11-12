081bf64c402da42ebf6f5939c2880dc39fa795a2
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-12 23:36:07 +0800

    Add tableCustomStyles for datatable ui

---
+++ src/@utils/tableCustomStyles.ts
@@ -0,0 +1,23 @@
+const tableCustomStyles = {
+  headCells: {
+    style: {
+      backgroundColor: "#c9a128",
+      fontSize: "15px",
+      color: "#ffffff",
+    },
+  },
+  cells: {
+    style: {
+      backgroundColor: "#ffffff",
+      fontSize: "15px",
+      borderBottom: "1px solid #b3b3b3",
+    },
+    table: {
+      style: {
+        backgroundColor: "transparent",
+      },
+    },
+  },
+};
+
+export { tableCustomStyles };
