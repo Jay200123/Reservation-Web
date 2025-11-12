c69c83b6bfa5c174c52d365b8b772edfe9428081
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-12 23:33:34 +0800

    Add ServicesTable page

--- src/@pages/ServicesTable/index.tsx
+++ src/@pages/ServicesTable/index.tsx
@@ -1,7 +1,7 @@
 export default function ServicesTable() {
     return (
-        <>
+        <div className="flex items-center justify-center min-h-screen">
             <h3>Services Table</h3>
-        </>
+        </div>
     )
 }
