3e68e2ebd4228739ac15dd40b1f38bad2218a9d8
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-27 21:52:41 +0800

    Add new state `service_rating` & serviceRating action

--- src/@types/slice/service.ts
+++ src/@types/slice/service.ts
@@ -1,11 +1,13 @@
 type ServiceState = {
   service_name: string;
   service_price: number;
+  service_rating: number;
 };
 
 type ServiceActions = {
   serviceNameFilter: (service_name: string) => void;
   servicePriceFilter: (service_price: number) => void;
+  serviceRating: (rating: number) => void;
 };
 
 type useServiceFilterSlice = ServiceState & ServiceActions;
